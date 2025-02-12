0ad2cabc3a1f42b469d8e8cf4d4cb40abc4329f1
nuriary <asp836@gmail.com>
2025-02-12 13:57:58 +0900

    Update index.js
    터미널 수정

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
