e6c159549ba660a5221846e48955d78346882fb4
nuriary <asp836@gmail.com>
2025-02-12 14:07:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
